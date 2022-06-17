2f511a05dda9385e51952225c2d506eda1f2ca75
IsaacJReay <isaacjacksonreay@gmail.com>
2022-06-17 14:31:52 +0700

    version 1.0: add implicit ssl and passive mode address and hostname option

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,9 @@
 FROM archlinux:latest
 COPY libpam_pwdfile-1.0-2-x86_64.pkg.tar.zst vsftpd-3.0.5-1-x86_64.pkg.tar.zst /
-COPY pacman.conf /etc/pacman.conf
-RUN yes | pacman -Syy && \
-    yes | pacman -U /libpam_pwdfile-1.0-2-x86_64.pkg.tar.zst /vsftpd-3.0.5-1-x86_64.pkg.tar.zst --overwrite="*" && \
-    yes | pacman -Scc && \
+RUN pacman -Syy && yes | pacman -U /libpam_pwdfile-1.0-2-x86_64.pkg.tar.zst /vsftpd-3.0.5-1-x86_64.pkg.tar.zst --overwrite="*" && \
     rm /libpam_pwdfile-1.0-2-x86_64.pkg.tar.zst  /vsftpd-3.0.5-1-x86_64.pkg.tar.zst && \
-    mkdir -p /etc/vsftpd /srv/vsftpd && \
-    useradd -d /srv/vsftpd vsftpd && \
-    chown vsftpd:vsftpd /srv/vsftpd && \
-    echo 'vsftpd: ALL\n' > /etc/hosts.allow
+    mkdir -p /etc/vsftpd && \
+    useradd -m -d /srv/vsftpd vsftpd 
 COPY --chmod=755 run_vsftpd.sh /usr/bin/
 COPY vsftpd.conf /etc/vsftpd.conf
 COPY vsftpd_pam /etc/pam.d/vsftpd
--- run_vsftpd.sh
+++ run_vsftpd.sh
@@ -2,9 +2,27 @@
 
 export PASV_MIN_PORT
 export PASV_MAX_PORT
+export PASV_ADDR_RESOLVE
+export PASV_ADDRESS
+export IMPLICIT_SSL
+export IMPLICIT_SSL_PORT
 
-sed -i "s/pasv_min_port=50000/pasv_min_port=${PASV_MIN_PORT}/g" /etc/vsftpd.conf
-sed -i "s/pasv_max_port=60100/pasv_max_port=${PASV_MAX_PORT}/g" /etc/vsftpd.conf
+[[ ! -z "${PASV_MIN_PORT}" ]] && sed -i "s/pasv_min_port=50000/pasv_min_port=${PASV_MIN_PORT}/g" /etc/vsftpd.conf
+[[ ! -z "${PASV_MAX_PORT}" ]] && sed -i "s/pasv_max_port=60100/pasv_max_port=${PASV_MAX_PORT}/g" /etc/vsftpd.conf
+[[ "${PASV_ADDR_RESOLVE}" == "YES" ]] && echo -e "\npasv_addr_resolve=YES\n" | tee -a /etc/vsftpd.conf
+[[ ! -z "${PASV_ADDRESS}" ]] && echo -e "\npasv_address=${PASV_ADDRESS}\n" | tee -a /etc/vsftpd.conf
+
+if [[ "${IMPLICIT_SSL}" == "YES" ]];
+then
+    echo -e "\nimplicit_ssl=YES\n" >> /etc/vsftpd.conf
+
+    if [[ ! -z "${IMPLICIT_SSL_PORT}" ]];
+    then
+        echo -e "\nlisten_port=${IMPLICIT_SSL_PORT}\n" >> /etc/vsftpd.conf
+    else 
+        echo -e "\nlisten_port=990\n" >> /etc/vsftpd.conf
+    fi
+fi
 
 NUM=1;
 USER_ARR=()
